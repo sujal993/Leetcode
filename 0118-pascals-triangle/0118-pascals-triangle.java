14a862fc5a5d24a1d9152a4fb338dc4e8a32e15f
sujal993 <112621052+sujal993@users.noreply.github.com>
2026-06-25 01:22:29 +0530

    Time: 0 ms (100%), Space: 43.3 MB (69.54%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,38 +1,20 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-
-        // This will store the entire Pascal's Triangle
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // Create each row one by one
-        for (int i = 0; i < numRows; i++) {
-
-            // Current row
-            List<Integer> row = new ArrayList<>();
-
-            // Every row starts with 1
-            row.add(1);
-
-            // Fill the middle elements
-            // i = row number (starting from 0)
-            for (int j = 1; j < i; j++) {
-
-                // Previous row
-                List<Integer> prev = ans.get(i - 1);
-
-                // Sum of the two numbers above
-                row.add(prev.get(j - 1) + prev.get(j));
-            }
-
-            // Every row except the first ends with 1
-            if (i > 0) {
-                row.add(1);
-            }
-
-            // Add the completed row to the answer
-            ans.add(row);
+        List<List<Integer>> list=new ArrayList<>();
+       for(int i=1;i<=numRows;i++){
+          list.add(generateRow(i));
+       } 
+       return list;
+    }
+    public List<Integer> generateRow(int rows){
+        List<Integer> list=new ArrayList<>();
+        int ans=1;
+        list.add(1);
+        for(int i=1;i<rows;i++){
+          ans*=(rows-i);
+          ans/=i;
+          list.add(ans);
         }
-
-        return ans;
+        return list;
     }
 }
