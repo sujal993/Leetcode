c8e255fb1308eef8043ab56722b742ac255faf82
sujal993 <112621052+sujal993@users.noreply.github.com>
2026-06-25 23:38:45 +0530

    Time: 623 ms (88.35%), Space: 0B (100%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,11 @@
+with cte as
+(
+    select *,
+            row_number() over(partition by email order by id) as rn
+    from person
+)
+delete from person
+where id in (select id
+from cte
+where rn > 1
+)  
